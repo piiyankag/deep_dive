6548c140ac920a25fe06917f035d2943c4933138
Yann Labour <yann@sugarsplashes.com>
2023-11-11 15:56:19 +0400

    make requirements optional for colab

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
+import os
 
-with open("requirements.txt") as f:
-    content = f.readlines()
-requirements = [x.strip() for x in content if "git+" not in x]
+requirements = []
 
-setup(name='deepdive',
-      version="0.0.1",
-      description="DeepDive Model (api_pred)",
-      license="MIT",
-      author="Priyanka Gunnoo, Yann Labour",
-      author_email="contact@lewagon.org",
-      install_requires=requirements,
-      packages=find_packages(),
-      # include_package_data: to install data from MANIFEST.in
-      include_package_data=True,
-      zip_safe=False)
+if os.path.exists("requirements.txt"):
+    with open("requirements.txt") as f:
+        content = f.readlines()
+    requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(
+    name='deepdive',
+    version="0.0.1",
+    description="DeepDive Model (api_pred)",
+    license="MIT",
+    author="Priyanka Gunnoo, Yann Labour",
+    author_email="contact@lewagon.org",
+    install_requires=requirements,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
+)
